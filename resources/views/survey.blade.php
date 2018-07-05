a7afd1b1fa760be750bb5de22025b9de94362b52
Patrick Farley <patrickfarley1994@yahoo.com>
2018-07-04 23:49:23 -0400

    fixed buttons

--- resources/views/survey.blade.php
+++ resources/views/survey.blade.php
@@ -79,5 +79,5 @@
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
 
-<script src="../assets/js/survey.js">
+<script src="/js/survey.js">
 </script>
--- resources/views/wall.blade.php
+++ resources/views/wall.blade.php
@@ -124,4 +124,4 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
 
 <!-- connecting js file  -->
-<script src="/assets/js/wall.js"></script>
+<script src="/js/wall.js"></script>
