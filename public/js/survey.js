a7f393a24ea767f98af65b6146596ca53787acf9
BETH-A <elizabeth78@me.com>
2018-07-15 15:20:30 -0400

    updated survey.js

--- public/js/survey.js
+++ public/js/survey.js
@@ -4,20 +4,20 @@ $("#survey-button").on("click", function (event) {
     event.preventDefault();
     console.log("clicked")
 
-    var a1 = $("#q1.form-control").val();
-    var a2 = $("#q2.form-control").val();
-    var a3 = $("#q3.form-control").val();
-    var a4 = $("#q4.form-control").val();
-    var a5 = $("#q5.form-control").val();
+    var movie = $("#q1.form-control").val();
+    var music = $("#q2.form-control").val();
+    var sport = $("#q3.form-control").val();
+    var food = $("#q4.form-control").val();
+    var activity = $("#q5.form-control").val();
     var id = $("#user_id").text();
     console.log(id);
 
     var survey = {
-        a1: a1,
-        a2: a2,
-        a3: a3,
-        a4: a4,
-        a5: a5,
+        movie: movie,
+        music: music,
+        sport: sport,
+        food: food,
+        activity: activity,
         UserId: id
     };
 
