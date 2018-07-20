c63f325a499c91e82b303187a040a43c172a3865
BETH-A <elizabeth78@me.com>
2018-07-20 15:06:50 -0400

    login blade

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="/img/connect.png" />
 
-    <title>Student Connect</title>
+    <title>Student Connect 2018 </title>
 
     <!-- Styling -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
