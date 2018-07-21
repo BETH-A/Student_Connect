58d9a32075f51e3cba54cbf2ade83f4b83ca19d3
BETH-A <elizabeth78@me.com>
2018-07-20 21:38:31 -0400

    session controller update

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -16,7 +16,7 @@ class SessionsController extends Controller
 
     public function create()
     {
-        return view('/wall');
+        return view('/session');
     }
 
     public function store()
