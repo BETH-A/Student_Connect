d48820f9803e83de74f7fd06ca2a0fcda846da24
BETH-A <elizabeth78@me.com>
2018-07-20 21:06:43 -0400

    api fix

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -16,7 +16,7 @@ class SessionsController extends Controller
 
     public function create()
     {
-        return view('session');
+        return view('/wall');
     }
 
     public function store()
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
-/*
-Route::get('/', 'LoginController@index');
 
-Eloquent model => User
-controller => LoginController
-migration => create_user_table
-
-php artisan make:model User - mc (makes all 3 files at once)
-*/
-/* routes:
- GET /posts = 
- GET /posts/create = form to create new
- post /posts = sends info to DB
- GET /posts/{id}/edit = edit form to update saved info
- GET /posts/{id} = view a single 
- PATCH /posts/{id} = updated edited info
- DELETE /posts/{id} = deletes resource from DB
-*/
 
  use App\Survey;
 
